794baee1ba9c8d9447581d42adc59229a7359401
Aeron01 <one.soulreaper@gmail.com>
2021-02-16 16:28:38 +0100

    Add 4 exercise

---
+++ convert.js
@@ -0,0 +1,28 @@
+let nb1 = 10
+let nb2 = 15
+let nb3 = 16
+let nb4 = 5005
+let nb5 = 52390903
+let txt1 = 'binaire'
+let txt2 = 'octal'
+let txt3 = 'terminal'
+console.log('Conversion en', txt1)
+console.log(nb1.toString(2))
+console.log(nb2.toString(2))
+console.log(nb3.toString(2))
+console.log(nb4.toString(2))
+console.log(nb5.toString(2))
+console.log('')
+console.log('Conversion en', txt2)
+console.log(nb1.toString(8))
+console.log(nb2.toString(8))
+console.log(nb3.toString(8))
+console.log(nb4.toString(8))
+console.log(nb5.toString(8))
+console.log('')
+console.log('Conversion en', txt3)
+console.log(nb1.toString(16))
+console.log(nb2.toString(16))
+console.log(nb3.toString(16))
+console.log(nb4.toString(16))
+console.log(nb5.toString(16))
