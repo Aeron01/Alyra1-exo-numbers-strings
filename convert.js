a7830043c728b4ae3879d3baaa11e9bf14380388
Aeron01 <one.soulreaper@gmail.com>
2021-02-17 11:50:56 +0100

    minor change

--- convert.js
+++ convert.js
@@ -6,6 +6,7 @@ let nb5 = 52390903
 let txt1 = 'binaire'
 let txt2 = 'octal'
 let txt3 = 'terminal'
+console.log('Les chiffres suivant :' + '\n' + `${nb1}\n${nb2}\n${nb3}\n${nb4}\n${nb5}\n\n`)
 console.log('Conversion en', txt1)
 console.log(nb1.toString(2))
 console.log(nb2.toString(2))
